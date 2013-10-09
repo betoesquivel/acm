7191a27a122f5c1de7fba01bfb21c9b5ffc85058
ppesq <betoesquivel>
2013-10-08 21:45:49 -0500

    I think I finished this problem

--- 1959Gifts.cpp
+++ 1959Gifts.cpp
@@ -16,11 +16,68 @@
  * =====================================================================================
  */
 #include <iostream>
+#include <list>
 
 using namespace std; 
 int main()
 {
+	int numberOfCows, budget;
+	cin>>numberOfCows>>budget;
+	
+	list<int> giftCosts, shippingCosts, totalCosts;
+	list<int>::iterator giftCosts_it, shippingCosts_it, totalCosts_it;
+	for(int cow_it = 0; cow_it<numberOfCows; cow_it++)
+	{
+		int cowGiftCost, cowShippingCost, cowGiftTotalCost;
+		cin>>cowGiftCost>>cowShippingCost;
+		cowGiftTotalCost = cowGiftCost+cowShippingCost;
 
+		giftCosts_it = giftCosts.begin();
+		shippingCosts_it = shippingCosts.begin();
+		totalCosts_it = totalCosts.begin();
+		while(totalCosts_it != totalCosts.end() && cowGiftTotalCost > *totalCosts_it)
+		{
+			giftCosts_it++;
+			shippingCosts_it++;
+			totalCosts_it++;
+		}
+		
+		if(totalCosts_it != totalCosts.end())
+		{
+			giftCosts.insert(giftCosts_it, cowGiftCost);
+			shippingCosts.insert(shippingCosts_it, cowShippingCost);
+			totalCosts.insert(totalCosts_it, cowGiftTotalCost);
+		}
+		else
+		{
+			giftCosts.push_back(cowGiftCost);
+			shippingCosts.push_back(cowShippingCost);
+			totalCosts.push_back(cowShippingCost);
+		}
+	}
+
+	int subTotal = 0, giftsAfforded = 0;
+        int mostExpensiveGift, tentativeDiscount;
+	//int discount;
+
+	giftCosts_it = giftCosts.begin();
+	totalCosts_it = totalCosts.begin();
+	while(totalCosts_it != totalCosts.end() && (subTotal-tentativeDiscount) <= budget)
+	{
+		subTotal += *totalCosts_it;
+		mostExpensiveGift = (*giftCosts_it > mostExpensiveGift) ? *giftCosts_it:mostExpensiveGift;
+		//discount = tentativeDiscount;
+		tentativeDiscount = mostExpensiveGift/2;
+
+		if((subTotal-tentativeDiscount)<=budget)
+		{
+			giftsAfforded++;
+			totalCosts_it++;
+			giftCosts_it++;
+		}
+	}
+	cout<<giftsAfforded-1<<endl;
 }
+
 
 
