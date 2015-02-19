5497cb3f65a2ced26e5ad2877dd8ce3b73d02d69
Beto Esquivel <ppbeto94@gmail.com>
2015-02-19 17:39:24 -0600

    About to change the algorithm to a method

--- 2900Lektira.cpp
+++ 2900Lektira.cpp
@@ -3,9 +3,77 @@
 
 using namespace std;
 
+// algorithm
+// Read sequentially the string until the most I can read depending on how many parts I have left
+// to pick.
+// While reading, if you found a smallest letter, read all other equal consecutive letters while counting.
+// This way I can provide a comparison of.
+
+string getCutPart(string s, int endIndex) {
+  string cut = "";
+  stack<char> inverse;
+
+  int min = 0, toPop = 0, minSize = 1;
+  inverse.push(s[0]);
+  for (int i = 1; i < endIndex; i++){
+    inverse.push(s[i]);
+    toPop += 1;
+
+    if (s[i] < s[min]){
+
+      min = i;
+      toPop = 0;
+
+      i += 1;
+      // collect all contiguous equal chars
+      while ( i < endIndex && s[i] == s[min] ){
+        inverse.push(s[0]);
+        min = i;
+        minSize++;
+        i++;
+      }
+
+    }else if ( s[i] == s[min] ) {
+      int tmp = i;
+      int tmpSize = 1;
+      i += 1;
+      // collect all contiguous equal chars
+      while ( i < endIndex && s[i] == s[min] ){
+        inverse.push(s[0]);
+        toPop++;
+        tmp = i;
+        tmpSize++;
+        i++;
+      }
+
+      if (tmpSize > minSize) {
+        minSize = tmpSize;
+        min = tmp;
+        toPop = 0;
+      }
+      i -= 1;
+
+    }
+
+  }
+
+  while ( toPop > 0 ){
+    inverse.pop();
+  }
+  while ( !inverse.empty() ){
+    cut += inverse.top();
+    inverse.pop();
+  }
+  return cut;
+
+}
+
+
+
 int main(int argc, const char *argv[])
 {
   std::ios_base::sync_with_stdio(false);
+
   string input, output = "";
   cin >> input;
 
@@ -17,20 +85,49 @@ int main(int argc, const char *argv[])
   while ( neededParts > 0 ){
 
     int smallestIndex = 0;
-    int elementsToPop = 0;
+    int smallestIndexSize = 1;
+    int elementsToPop = 1;
     int j;
-    for (j = 0; j < ( len-neededParts ); j++) {
+     inversePart.push(input[j]);
+     elementsToPop += 1;
+    for (j = 1; j < ( len-neededParts ); j++) {
        inversePart.push(input[j]);
        elementsToPop += 1;
-       if (input[j] <= input[smallestIndex]) {
-         if ( !( j > 0  && input[j-1] != input[j] && input[j] == input[smallestIndex] )) {
-           smallestIndex = j;
-           elementsToPop = 0;
+       if (input[j] < input[smallestIndex]) {
+         smallestIndex = j;
+         elementsToPop = 0;
+         smallestIndexSize = 1;
+         // eat all the same contiguous letters
+         for ( j = j+1; j < (len - neededParts) ; j++){
+             if(input[j] != input[smallestIndex]) { j-=1; break; }
+             smallestIndexSize+=1;
+             smallestIndex = j;
+             inversePart.push(input[j]);
+             elementsToPop += 1;
+         }
+       }else if (input[j] == input[smallestIndex]){
+         // found an equal smallest letter
+         // Now I have to keep the one that repeats itself the most
+         int indexRepetitions = 1;
+         int tempIndex = j;
+         for ( j = j+1; j < (len - neededParts) ; j++){
+             if(input[j] != input[tempIndex]) { break; }
+             indexRepetitions+=1;
+             tempIndex = j;
+             inversePart.push(input[j]);
+             elementsToPop += 1;
+         }
+         if (indexRepetitions > smallestIndexSize) {
+             smallestIndex = j - 1;
+             smallestIndexSize = indexRepetitions;
+             elementsToPop = 0;
          }
 
        }
     }
     // dcbagfekjih
+    // anakonda
+    // bbabbbdddxddddaa
     while ( elementsToPop > 0 ){
 
       inversePart.pop();
